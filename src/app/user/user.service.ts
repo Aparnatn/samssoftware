3017e7df1c65a80dabdf7d63f1f4cb75c3bf77cb
aparna <aparnaneelakandhan135@gmail.com>
2021-11-24 10:00:22 +0530

    Compan  commits

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -83,7 +83,7 @@ export class UserService {
   }
 
   ledger(data: LedgerRequest): Observable<LedgerResponse> {
-    return this.http.post<LedgerResponse>(`${this.apiUrl}/Sam/ledg`, data, {
+    return this.http.post<LedgerResponse>(`${this.apiUrl}/Sam/ledgercreate`, data, {
       // observe: 'response',
       // withCredentials: true
     });
